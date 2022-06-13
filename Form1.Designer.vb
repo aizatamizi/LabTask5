bd84dc4772db77cb3a26c5946188ee69d811a528
ASUS <ASUS@LAPTOP-UDHPNQ86>
2022-06-13 23:27:08 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ListBox = New System.Windows.Forms.ListBox()
+        Me.txtfirstname = New System.Windows.Forms.TextBox()
+        Me.txtlastname = New System.Windows.Forms.TextBox()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.txtIC = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.label = New System.Windows.Forms.Label()
+        Me.lblname = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Agency FB", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(71, 85)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 28)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "First Name :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Agency FB", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(71, 140)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 28)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Last Name :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Agency FB", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(71, 194)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 28)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Username :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Agency FB", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(71, 249)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 28)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "IC Number :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Agency FB", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(77, 332)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(90, 28)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Name List:"
+        '
+        'ListBox
+        '
+        Me.ListBox.FormattingEnabled = True
+        Me.ListBox.ItemHeight = 16
+        Me.ListBox.Location = New System.Drawing.Point(75, 380)
+        Me.ListBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.ListBox.Name = "ListBox"
+        Me.ListBox.Size = New System.Drawing.Size(654, 164)
+        Me.ListBox.TabIndex = 10
+        '
+        'txtfirstname
+        '
+        Me.txtfirstname.Location = New System.Drawing.Point(196, 91)
+        Me.txtfirstname.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtfirstname.Name = "txtfirstname"
+        Me.txtfirstname.Size = New System.Drawing.Size(275, 22)
+        Me.txtfirstname.TabIndex = 11
+        '
+        'txtlastname
+        '
+        Me.txtlastname.Location = New System.Drawing.Point(196, 147)
+        Me.txtlastname.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtlastname.Name = "txtlastname"
+        Me.txtlastname.Size = New System.Drawing.Size(275, 22)
+        Me.txtlastname.TabIndex = 12
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(196, 201)
+        Me.txtusername.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(275, 22)
+        Me.txtusername.TabIndex = 13
+        '
+        'txtIC
+        '
+        Me.txtIC.Location = New System.Drawing.Point(196, 256)
+        Me.txtIC.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtIC.Name = "txtIC"
+        Me.txtIC.Size = New System.Drawing.Size(275, 22)
+        Me.txtIC.TabIndex = 14
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.Silver
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.ForeColor = System.Drawing.Color.Red
+        Me.btnAdd.Location = New System.Drawing.Point(629, 85)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(100, 41)
+        Me.btnAdd.TabIndex = 15
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.Silver
+        Me.btnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.ForeColor = System.Drawing.Color.Red
+        Me.btnReset.Location = New System.Drawing.Point(629, 147)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(100, 41)
+        Me.btnReset.TabIndex = 16
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'label
+        '
+        Me.label.AutoSize = True
+        Me.label.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label.ForeColor = System.Drawing.Color.Black
+        Me.label.Location = New System.Drawing.Point(498, 259)
+        Me.label.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.label.Name = "label"
+        Me.label.Size = New System.Drawing.Size(181, 16)
+        Me.label.TabIndex = 17
+        Me.label.Text = "Without Dash (-), 12 Digit "
+        '
+        'lblname
+        '
+        Me.lblname.AutoSize = True
+        Me.lblname.Font = New System.Drawing.Font("Agency FB", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblname.Location = New System.Drawing.Point(78, 573)
+        Me.lblname.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblname.Name = "lblname"
+        Me.lblname.Size = New System.Drawing.Size(64, 28)
+        Me.lblname.TabIndex = 18
+        Me.lblname.Text = "Name :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(796, 622)
+        Me.Controls.Add(Me.lblname)
+        Me.Controls.Add(Me.label)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtIC)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.txtlastname)
+        Me.Controls.Add(Me.txtfirstname)
+        Me.Controls.Add(Me.ListBox)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ListBox As ListBox
+    Friend WithEvents txtfirstname As TextBox
+    Friend WithEvents txtlastname As TextBox
+    Friend WithEvents txtusername As TextBox
+    Friend WithEvents txtIC As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents label As Label
+    Friend WithEvents lblname As Label
+End Class
